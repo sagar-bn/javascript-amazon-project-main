dfb9aebc65f54674abb4d7f08f0706dc51be88ed
sagar-bn <bnsagar27@gmail.com>
2025-07-28 11:36:51 +0530

    make add to cart with select quantity feature

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -19,11 +19,11 @@ products.forEach((products)=>{
           </div>
 
           <div class="product-price">
-            $${products.priceCents/100}
+            $${(products.priceCents/100).toFixed(2)}
           </div>
 
-          <div class="product-quantity-container">
-            <select>
+          <div class="product-quantity-container" >
+            <select class="js-select-quantity-${products.id}" >
               <option selected value="1">1</option>
               <option value="2">2</option>
               <option value="3">3</option>
@@ -50,38 +50,78 @@ products.forEach((products)=>{
           </button>
         </div>
 `
+//console.log(`js-select-quantity-${products.id}`);
 });
 document.querySelector('.js-products').innerHTML=HTML;
 
+
+let MatchingProduct;
 document.querySelectorAll('.js-add-to-cart')
-   .forEach((button)=>{
-      button.addEventListener('click',()=>{
-       
-      productId= button.dataset.productsId;
-      
-       let isMatching;
-      cart.forEach((item)=>{
-         if(productId === item.productId){
-            isMatching = item;
-         }
-        });
-        if(!isMatching){
-          cart.push({
-          productId:productId,
-          quantity:1
-          });
-        }
-        else{
-          isMatching.quantity +=1;
-        }
-      
-        let cartQuantity=0;
+.forEach((button)=>{
+  button.addEventListener('click',()=>{
+    const productId = button.dataset.productsId; 
+   const select = document.querySelector(`.js-select-quantity-${productId}`);
+  const selectedQuantity=Number(select.value);
+   select.value=1;
+ 
+
+
+   cart.forEach((item)=>{
+    if(productId === item.productId){
+      MatchingProduct= item;
+    }
+   });
+   if(MatchingProduct){
+    MatchingProduct.quantity += selectedQuantity;
+   }
+   else{
+     cart.push({
+    productId:productId ,
+    quantity : selectedQuantity
+     });}
+      let cartQuantity=0;
       cart.forEach((item)=>{
         cartQuantity += item.quantity;
       });
 
       document.querySelector('.js-cart-quantity').innerHTML=cartQuantity;
      
-      })
-   });
+  console.log(cart);
+  });
+});
+
+
+
+
+// document.querySelectorAll('.js-add-to-cart')
+//    .forEach((button)=>{
+//       button.addEventListener('click',()=>{
+       
+//       productId= button.dataset.productsId;
+      
+//        let isMatching;
+//       cart.forEach((item)=>{
+//          if(productId === item.productId){
+//             isMatching = item;
+//          }
+//         });
+//         if(!isMatching){
+//           cart.push({
+//           productId:productId,
+//           quantity:1
+//           });
+//         }
+//         else{
+//           isMatching.quantity +=1;
+//         }
+      
+//         let cartQuantity=0;
+//       cart.forEach((item)=>{
+//         cartQuantity += item.quantity;
+//       });
+
+//       document.querySelector('.js-cart-quantity').innerHTML=cartQuantity;
+     
+//       })
+//    });
   

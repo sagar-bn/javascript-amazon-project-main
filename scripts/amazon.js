93d85df2614e975519e2d56a4107b47b7473a44b
sagar-bn <bnsagar27@gmail.com>
2025-07-28 18:01:49 +0530

    load from js, data added in cart,currencyFormating utile file,checkout js created

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,5 +1,6 @@
 import {cart,addToCart} from '../data/cart.js';
-import {products} from '../data/products.js'
+import {products} from '../data/products.js';
+import {currencyFormating} from './utils/utitility.js';
 let HTML='';
 products.forEach((products)=>{
  HTML += `<div class="product-container ">
@@ -21,7 +22,7 @@ products.forEach((products)=>{
           </div>
 
           <div class="product-price">
-            $${(products.priceCents/100).toFixed(2)}
+            $${currencyFormating(products.priceCents)}
           </div>
 
           <div class="product-quantity-container" >
---
+++ scripts/utils/utitility.js
@@ -0,0 +1,3 @@
+export function currencyFormating(priceCents){
+  return (priceCents/100).toFixed(2);
+}

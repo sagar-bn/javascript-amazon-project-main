7417e1a41dbcca864e8f499c66c80ff866540d9e
sagar-bn <bnsagar27@gmail.com>
2025-07-28 08:56:13 +0530

    make cartQuantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -75,9 +75,13 @@ document.querySelectorAll('.js-add-to-cart')
           isMatching.quantity +=1;
         }
       
-      
+        let cartQuantity=0;
+      cart.forEach((item)=>{
+        cartQuantity += item.quantity;
+      });
 
-      console.log(cart);
+      document.querySelector('.js-cart-quantity').innerHTML=cartQuantity;
+     
       })
    });
   

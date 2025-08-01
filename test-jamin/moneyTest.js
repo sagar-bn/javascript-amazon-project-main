5a8221bb3c73a4dd944629d8c685151fa63f4295
sagar-bn <bnsagar27@gmail.com>
2025-08-01 12:28:25 +0530

    moved default test add moneytest

---
+++ test-jamin/moneyTest.js
@@ -0,0 +1,15 @@
+import { currencyFormating } from '../scripts/utils/utitility.js';
+
+describe('test suite : format currency', () => {
+  it('converts cents into dollers', () => {
+    expect(currencyFormating(2005)).toEqual('20.05');
+  });
+
+  it('works with 0', () => {
+    expect(currencyFormating(0)).toEqual('0.00');
+  });
+
+  it('rounds up to the nearst cent', () => {
+    expect(currencyFormating(2000.5)).toEqual('20.01');
+  });
+});

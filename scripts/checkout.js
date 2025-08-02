f0f347bd7a011a7ba263bc3c0a30466c17736793
sagar-bn <bnsagar27@gmail.com>
2025-08-02 14:05:27 +0530

    class without constructor understand how object is generated

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,10 +1,9 @@
 import {renderOrderSummary} from './checkout/OrderSummary.js';
 import { renderpaymentSummary } from './checkout/PaymentSummary.js';
-import  '../data/cart-oop.js';
+import  '../data/cart-class.js';
 
 
 renderOrderSummary();
 renderpaymentSummary();
-
 
 
